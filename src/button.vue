2f9c6b21342c880a4881d0a3c92defd0b064df85
plushao <767832499@qq.com>
2019-10-21 22:23:34 +0800

    button 单文件化。 未解决：页面不显示button 按钮

---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button class="v-button">按钮</button>
+</template>
+
+<script>
+	export default {
+		name: "button"
+	}
+</script>
+
+<style  lang="scss">
+  .v-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      background-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+</style>
